fdcfb874ff86b54cfa5723acb0fe3b39ec36ed0f
Jessie Hildebrandt <jessieh@jessieh.net>
2019-04-11 03:36:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# mood-one-theme
+### Version 1.0.0
+
+## About
+
+`mood-one-theme` is a dark color scheme that aims to replicate some of the features of the Doom One theme.
+
+## Features
+
+* Beautiful dark color scheme inspired by the Doom One theme
+
+* Custom fringe bitmaps for diff-hl and flycheck
+
+* Lightweight with no dependencies
+
+## Configuration
+
+To enable the custom fringe bitmaps, place this in your configuration file after loading the package:
+
+`(mood-one-theme-enable-fringe-bmps)`
+
+## Known Issues
+
+* No known issues.
+
+If you experience any issues with this package, please
+[open an issue](https://gitlab.com/jessieh/mood-line/issues/new)
+on the issue tracker.
+
+Suggestions for improvements and feature requests are always appreciated, as well!
---
+++ mood-one-theme-fringe.el
@@ -0,0 +1,63 @@
+;;; mood-one-theme-fringe.el --- Fringe bitmap definitions for the Mood One theme. -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+;; This file is responsible for defining custom fringe bitmaps
+;; of supported packages for the Mood One theme.
+
+;;; Code:
+
+;;
+;; Define fringe bitmaps
+;;
+
+(eval-after-load 'mood-one-theme
+  (lambda ()
+
+    ;; Define diff-hl fringe bitmaps
+    (eval-after-load 'diff-hl
+      (lambda ()
+        (setq diff-hl--custom-bmp
+              (define-fringe-bitmap 'mood-one-theme--diff-hl-bmp
+                (vector #b11100000)
+                1 8
+                '(center t)))
+        (defun mood-one-theme--diff-hl-bmp-fn (_type _pos)
+          diff-hl--custom-bmp)
+        (setq diff-hl-fringe-bmp-function #'mood-one-theme--diff-hl-bmp-fn)))
+
+    ;; Define flycheck fringe bitmaps
+    (eval-after-load 'flycheck
+      (lambda()
+        (define-fringe-bitmap 'mood-one-theme--flycheck-bmp
+          (vector #b11100000
+                  #b11110000
+                  #b11111000
+                  #b11111100
+                  #b11111000
+                  #b11110000
+                  #b11100000))
+        (flycheck-define-error-level 'error
+          :severity 2
+          :overlay-category 'flycheck-error-overlay
+          :fringe-bitmap 'mood-one-theme--flycheck-bmp
+          :fringe-face 'flycheck-fringe-error)
+        (flycheck-define-error-level 'warning
+          :severity 1
+          :overlay-category 'flycheck-warning-overlay
+          :fringe-bitmap 'mood-one-theme--flycheck-bmp
+          :fringe-face 'flycheck-fringe-warning)
+        (flycheck-define-error-level 'info
+          :severity 0
+          :overlay-category 'flycheck-info-overlay
+          :fringe-bitmap 'mood-one-theme--flycheck-bmp
+          :fringe-face 'flycheck-fringe-info)
+        ))))
+
+;;
+;; Provide mood-one-theme-fringe
+;;
+
+(provide 'mood-one-theme-fringe)
+
+;;; mood-one-theme-fringe.el ends here
