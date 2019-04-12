7b2cc45a2a8b5459f7c43442b97ee81fa269606f
Jessie Hildebrandt <jessieh@jessieh.net>
2019-04-12 04:04:32 -0400

    Add theme folder registration

--- mood-one-theme.el
+++ mood-one-theme.el
@@ -670,6 +670,15 @@
   (require 'mood-one-theme-fringe))
 
 ;;
+;; Register theme folder location
+;;
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+;;
 ;; Provide mood-one-theme
 ;;
 
